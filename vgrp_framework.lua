1c2f867ae358d0f3f08bceba5b4645d3d1bc6e74
vgzinhodev <vgdesignerprod@gmail.com>
2026-08-15 01:51:53 -0300

    Update vgrp_framework.lua

--- vgrp_framework.lua
+++ vgrp_framework.lua
@@ -10,7 +10,7 @@
 
 Caso tenha dúvidas consulte: 
 Discord: https://discord.gg/NjeRXA475g
-Documentação para este script: https://docs.vgzinhostore.com 2345678 nine
+Documentação para este script: https://docs.vgzinhostore.com :)
 ]]
 
 ------ VGroup Core System Export
